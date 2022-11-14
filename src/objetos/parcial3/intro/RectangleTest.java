8234f8f6dc6ac56c49bfaed57bd1d7d711bfd2c3
Christian-ChavezS <A330107@alumnos.uaslp.mx>
2022-11-13 22:35:59 -0600

    Tarea 2_Parcial 3:  Intro Unit Testing Alumno: Chavez Sanchez Christian José.

---
+++ src/objetos/parcial3/intro/RectangleTest.java
@@ -0,0 +1,26 @@
+package objetos.parcial3.intro;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class RectangleTest {
+
+    @Test
+    public void testArea()
+    {
+        Rectangle rectangle = new Rectangle(8, 4);
+        double area = rectangle.getArea();
+        Assertions.assertEquals(32, area);
+    }
+
+    @Test
+    public void testPerimeter()
+    {
+        Rectangle rectangle = new Rectangle(8, 4);
+        double perimeter = rectangle.getPerimeter();
+        Assertions.assertEquals(24, perimeter);
+    }
+
+
+
+}
